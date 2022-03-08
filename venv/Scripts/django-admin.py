d937deed51352eabc5de29f0ee88e309439f67ff
@Omollo_Caleb <omollo_developer@outlook.com>
2022-03-08 15:57:02 +0300

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\OMOLLO C.O\Documents\New folder\New folder (6)\django-quiz-app-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
